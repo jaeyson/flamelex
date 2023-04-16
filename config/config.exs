2de1e11c3f80cf83ec4f83e3451238467e13ec0c
JediLuke <jediluke@gitaccount.com>
2023-04-16 00:50:39 -0500

    we only really need one config file

--- config/config.exs
+++ config/config.exs
@@ -1,22 +1,21 @@
 import Config
 
-config :elixir,
-  # https://hexdocs.pm/elixir/DateTime.html#module-time-zone-database
-  :time_zone_database, Tzdata.TimeZoneDatabase
-
-config :scenic,
-  :assets, module: Flamelex.App.Scenic.Assets
-
 config :flamelex,
   :key_mapping, Flamelex.KeyMappings.VimClone
 
-# remove superfluous newline characters from logs
-# see: https://elixirforum.com/t/why-does-logger-output-in-iex-have-to-have-an-empty-line-after-every-line-logged/21822/4
-config :logger,
-  :console, format: "$time $metadata[$level] $levelpad$message\n"
+memex_env = "Paracelcus"
+memex_base_dir = "/Users/luke/memex" # this directory is where we store all Memex related data, for all environments
 
-# https://github.com/otobus/event_bus/wiki/Creating-(Registering)-Topics
+config :memelex,
+  active?: false,
+  environment: %{
+    name: memex_env,
+    memex_directory: "#{memex_base_dir}/#{memex_env}",
+    backups_directory: "#{memex_base_dir}/backups/#{memex_env}"
+  }
+
 config :event_bus,
+  # https://github.com/otobus/event_bus/wiki/Creating-(Registering)-Topics
   topics: [
     :general,         # This topic is used by Fluxus, it's for updating internal Fluxus state by firing actions #TODO rename this to `actions`?
     :user_input,      # This topic is for transmitting user input throughout the application, to the appropriate listeners, which will likely in turn fire off Fluxus actions as a result of that input
@@ -24,6 +23,15 @@ config :event_bus,
     :interrupts       # The idea behind this topic is to handle external interrupts, e.g. perhaps we will add email as a feature to Flamelex, well getting an email might go on the interrupts channel
   ]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+config :elixir,
+  # https://hexdocs.pm/elixir/DateTime.html#module-time-zone-database
+  :time_zone_database, Tzdata.TimeZoneDatabase
+
+config :scenic,
+  :assets, module: Flamelex.App.Scenic.Assets
+
+config :logger,
+  level: :debug,
+  truncate: :infinity,
+  console:
+    [format: "$time $metadata[$level] $levelpad$message\n"] # remove superfluous newline characters from logs, see: https://elixirforum.com/t/why-does-logger-output-in-iex-have-to-have-an-empty-line-after-every-line-logged/21822/4
