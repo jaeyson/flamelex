731d30ede05f776c63365bb66e31abea97936d60
JediLuke <jediluke@gitaccount.com>
2024-10-06 17:19:34 -0500

    this has been commented out for a while now, I think we need to put it back in cause the menubar uses it

--- lib/api/flamelex_api.ex
+++ lib/api/flamelex_api.ex
@@ -1,75 +1,76 @@
-defmodule Flamelex do
-  @moduledoc """
-  The main interface to the Flamelex application.
+# defmodule Flamelex do
+#   @moduledoc """
+#   The main interface to the Flamelex application.
 
-  Note that in order to have these functions automatically
-  available in the MenuBar, they need to be in a module called
-  `Flamelex.API` - but this is less desirable for users calling
-  the functions directly in the command line (where I want to
-  be able to say `Flamelex.temet_nosce` rather than the more
-  cumbersome `Flamelex.API.temet_nosce`) - the workaround is to
-  delegate functions here to Flamelex.API
-  """
+#   Note that in order to have these functions automatically
+#   available in the MenuBar, they need to be in a module called
+#   `Flamelex.API` - but this is less desirable for users calling
+#   the functions directly in the command line (where I want to
+#   be able to say `Flamelex.temet_nosce` rather than the more
+#   cumbersome `Flamelex.API.temet_nosce`) - the workaround is to
+#   delegate functions here to Flamelex.API
+#   """
 
-  # NOTE if I ever figure out some cool way of doing this
-  # automatically, post it back here https://elixirforum.com/t/batch-delegate-functions-to-a-module/25046
-  @api Flamelex.API
+#   # NOTE if I ever figure out some cool way of doing this
+#   # automatically, post it back here https://elixirforum.com/t/batch-delegate-functions-to-a-module/25046
+#   @api Flamelex.API
 
-  defdelegate temet_nosce(), to: @api
-end
+#   defdelegate temet_nosce(), to: @api
+# end
 
-defmodule Flamelex.API do
-  use Flamelex.Lib.ProjectAliases
+# defmodule Flamelex.API do
+#   use Flamelex.Lib.ProjectAliases
 
-  @doc """
-  `Know Thyself`
+#   @doc """
+#   `Know Thyself`
 
-  Use this function to recompile, reload and restart the `Flamelex` application.
+#   Use this function to recompile, reload and restart the `Flamelex` application.
 
-  https://www.youtube.com/watch?v=kl0rqoRbzzU
+#   https://www.youtube.com/watch?v=kl0rqoRbzzU
 
-  Flamelex is an interactive thinking-space. It is intended to be edited
-  on by the user, and incorporate changes to it's own codebase. It is a
-  more refined version of the original Lisp machine. When you make
-  changes in your Flamelex code/environment (try to start thinking of those
-  two as the same thing), sometimes you need to (safely!) shut-down the
-  application & restart it, without losing any state. That is what this
-  function is for. Except the keeping state part, that doesn't work yet!
-  """
-  def temet_nosce do
-    quit()
+#   Flamelex is an interactive thinking-space. It is intended to be edited
+#   on by the user, and incorporate changes to it's own codebase. It is a
+#   more refined version of the original Lisp machine. When you make
+#   changes in your Flamelex code/environment (try to start thinking of those
+#   two as the same thing), sometimes you need to (safely!) shut-down the
+#   application & restart it, without losing any state. That is what this
+#   function is for. Except the keeping state part, that doesn't work yet!
+#   """
 
-    IO.puts("\n#{__MODULE__} recompiling...")
-    IEx.Helpers.recompile()
+#   # def temet_nosce do
+#   #   quit()
 
-    IO.puts("\n#{__MODULE__} starting...\n")
-    Application.start(:flamelex)
-  end
+#   #   IO.puts("\n#{__MODULE__} recompiling...")
+#   #   IEx.Helpers.recompile()
 
-  def quit do
-    IO.puts("\n#{__MODULE__} stopping...")
-    Application.stop(:flamelex)
-    IO.puts("\n#{__MODULE__} Done.")
-  end
+#   #   IO.puts("\n#{__MODULE__} starting...\n")
+#   #   Application.start(:flamelex)
+#   # end
 
-  def redraw_gui do
-    # shuts down scenc, starts it & gets GUI controller to attempt to re-draw
-    # from scratch
-    raise "not implemented"
-  end
+#   def quit do
+#     IO.puts("\n#{__MODULE__} stopping...")
+#     Application.stop(:flamelex)
+#     IO.puts("\n#{__MODULE__} Done.")
+#   end
 
-  @doc """
-  #TODO
-  Increase or decrease the logging output of Flamelex during runtime.
-  """
-  def set_log_level(:debug) do
-    raise "How do I set the log level??"
-  end
+#   def redraw_gui do
+#     # shuts down scenc, starts it & gets GUI controller to attempt to re-draw
+#     # from scratch
+#     raise "not implemented"
+#   end
 
-  @doc """
-  Trigger help for the user.
-  """
-  def help do
-    raise "no help to be found :("
-  end
-end
+#   @doc """
+#   #TODO
+#   Increase or decrease the logging output of Flamelex during runtime.
+#   """
+#   def set_log_level(:debug) do
+#     raise "How do I set the log level??"
+#   end
+
+#   @doc """
+#   Trigger help for the user.
+#   """
+#   def help do
+#     raise "no help to be found :("
+#   end
+# end
