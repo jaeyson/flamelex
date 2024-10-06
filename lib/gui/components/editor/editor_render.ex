76808c1d35698e3123eede1fd32b42b025ad5f1d
JediLuke <jediluke@gitaccount.com>
2024-10-06 17:05:41 -0500

    quillex drawing the buffer GUI

--- lib/gui/components/editor/editor_render.ex
+++ lib/gui/components/editor/editor_render.ex
@@ -10,36 +10,24 @@ defmodule Flamelex.GUI.Component.Editor.Render do
   # Add this alias
   alias ScenicWidgets.TextPad.CursorCaret
 
-  @no_limits_to_tomorrow "~ The only limit to our realization of tomorrow is our doubts of today ~"
-
-  @typewriter %{
-    text: :black,
-    slate: :white
-  }
-
-  @cauldron %{
-    text: :white,
-    slate: :medium_slate_blue
-  }
-
   def go(%Widgex.Frame{} = frame, %State{} = state) do
-    text = @no_limits_to_tomorrow
-    font_size = 24
-    font_name = :ibm_plex_mono
+    # text = @no_limits_to_tomorrow
+    # font_size = 24
+    # font_name = :ibm_plex_mono
 
-    # TODO this is a little cheeky, should pass it through via the state somehow...
-    font_metrics = Flamelex.Fluxus.RadixStore.get().fonts.ibm_plex_mono.metrics
-    ascent = FontMetrics.ascent(font_size, font_metrics)
+    # # TODO this is a little cheeky, should pass it through via the state somehow...
+    # font_metrics = Flamelex.Fluxus.RadixStore.get().fonts.ibm_plex_mono.metrics
+    # ascent = FontMetrics.ascent(font_size, font_metrics)
 
-    font = %{
-      name: font_name,
-      size: font_size,
-      metrics: font_metrics
-    }
+    # font = %{
+    #   name: font_name,
+    #   size: font_size,
+    #   metrics: font_metrics
+    # }
 
-    colors = @cauldron
+    # colors = @cauldron
 
-    buf = hd(state.buffers)
+    buf_ref = hd(state.buffers)
 
     # def add_component_to_graph(graph, component_module, component_pid, opts) do
     #   graph
@@ -50,33 +38,37 @@ defmodule Flamelex.GUI.Component.Editor.Render do
 
     # |> MyApp.BufferServer.add_to_graph(%{pid: buf.pid}, [])
 
+    # TODO here is where we should add a Quillex.GUI.Buffer.add_to_graph
+
     graph =
       Graph.build()
-      |> Primitives.group(
-        fn graph ->
-          graph
-          |> Draw.background(frame, colors.slate)
-          |> Primitives.text(
-            text,
-            font_size: font_size,
-            font: font_name,
-            fill: colors.text,
-            translate: {10, ascent + 10}
-            # translate: {10, 10}
-          )
-          |> Flamelex.GUI.Component.Editor.CursorCaret.add_to_graph(
-            %{
-              buffer_uuid: buf.uuid,
-              coords: {10, 10},
-              height: font_size,
-              mode: :cursor,
-              font: font
-            },
-            id: :cursor
-          )
-        end,
-        translate: frame.pin.point
-      )
+      |> Quillex.GUI.Buffer.draw(frame, buf_ref)
+
+    # |> Primitives.group(
+    #   fn graph ->
+    #     graph
+    #     |> Draw.background(frame, colors.slate)
+    #     |> Primitives.text(
+    #       text,
+    #       font_size: font_size,
+    #       font: font_name,
+    #       fill: colors.text,
+    #       translate: {10, ascent + 10}
+    #       # translate: {10, 10}
+    #     )
+    #     |> Flamelex.GUI.Component.Editor.CursorCaret.add_to_graph(
+    #       %{
+    #         buffer_uuid: buf.uuid,
+    #         coords: {10, 10},
+    #         height: font_size,
+    #         mode: :cursor,
+    #         font: font
+    #       },
+    #       id: :cursor
+    #     )
+    #   end,
+    #   translate: frame.pin.point
+    # )
 
     graph
   end
