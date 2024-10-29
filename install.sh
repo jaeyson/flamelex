0fb4c73add1bf9fb7d3b334b67fbfacfb4e69ea9
JediLuke <jediluke@gitaccount.com>
2024-10-29 11:25:39 -0500

    one day have this

---
+++ install.sh
@@ -0,0 +1,13 @@
+Users can curl this & run it to install flamelex
+
+- pull git repo
+- install dependencies (including external deps e.g. repgrip)
+- build flamelex
+- install flamelex (flx command in command line)
+
+I Guess these ones should be in app...
+
+- (optional) create a local memex
+- (optional) sign up for flamelex Cloud
+- (optional) opt-in to answer surveys periodically - this helps us build better product and improve user experience, and helps us understand how you use flamelex / useful data we might sell / companies might pay us to collect - but it helps the project out
+- (optional) opt-in to telemetry
