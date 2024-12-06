ae1b24a7e8e868913184a7cb895a59a6f6b32c89
JediLuke <jediluke@gitaccount.com>
2024-12-05 23:06:46 -0800

    in pubsub dont broadcasdt back to our self when we broadcast

--- lib/utils/pubsub.ex
+++ lib/utils/pubsub.ex
@@ -2,22 +2,26 @@ defmodule Flamelex.Lib.Utils.PubSub do
   @registrar_proc Flamelex.Fluxus.PubSub
   @topic :radix_state_changes
 
-  def subscribe, do: subscribe(topic: @topic)
+  def subscribe do
+    IO.puts "DEPRECATE THIS just always pass in the bloody topic when you PubSUIb.subscribe"
+    subscribe(topic: @topic)
+  end
 
   def subscribe(topic: t) do
     {:ok, _} = Registry.register(@registrar_proc, t, [])
     :ok
   end
 
-  def broadcast(state_change: chng) do
-    Registry.dispatch(@registrar_proc, @topic, fn entries ->
-      for {pid, _} <- entries, do: send(pid, {:state_change, chng})
-    end)
-  end
+  # def broadcast(state_change: chng) do
+  #   Registry.dispatch(@registrar_proc, @topic, fn entries ->
+  #     for {pid, _} <- entries, do: send(pid, {:state_change, chng})
+  #   end)
+  # end
 
   def broadcast(topic: topic, msg: msg) do
     Registry.dispatch(@registrar_proc, topic, fn entries ->
-      for {pid, _} <- entries, do: send(pid, msg)
+      # != self is supposed to prevent looped messages
+      for {pid, _} <- entries, pid != self(), do: send(pid, msg)
     end)
   end
 end
