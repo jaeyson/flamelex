e592d2cc530d95ebe82d48372fdb292372a84e58
JediLuke <jediluke@gitaccount.com>
2025-04-12 19:47:06 -0700

    more fonts

--- lib/assets.ex
+++ lib/assets.ex
@@ -8,9 +8,10 @@ defmodule Flamelex.App.Scenic.Assets do
       # emoji: "fonts/Noto_Emoji/static/NotoEmoji-Regular.ttf",
       # music: "fonts/Noto_Music/NotoMusic-Regular.ttf",
       noto_sans: "fonts/Noto_Sans/static/NotoSans-Regular.ttf",
-      # symbols: "fonts/Noto_Sans_Symbols/static/NotoSansSymbols-Regular.ttf",
+      egtyptian_heiroglyphs: "fonts/Noto_Sans_Egyptian_Hieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf",
       # symbols_2: "fonts/Noto_Sans_Symbols_2/NotoSansSymbols2-Regular.ttf",
-      meroitic: "fonts/Noto_Sans_Meroitic/NotoSansMeroitic-Regular.ttf"
-      # ibm_plex_mono: "fonts/IBM_Plex_Mono/IBMPlexMono-Regular.ttf"
+      noto_sans_semi_condensed_medium_italic: "fonts/Noto_Sans/static/NotoSans_SemiCondensed-MediumItalic.ttf",
+      meroitic: "fonts/Noto_Sans_Meroitic/NotoSansMeroitic-Regular.ttf",
+      noto_sans_symbols: "fonts/Noto_Sans_Symbols/static/NotoSansSymbols-Regular.ttf"
     ]
 end
